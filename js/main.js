0b7919cdd0e84e4ced66a318217cef3c1fe8e1ed
mehamasum <mehamasum@gmail.com>
2017-06-23 12:10:02 +0600

    url custom attach name fix

--- js/main.js
+++ js/main.js
@@ -224,6 +224,10 @@ TrelloPowerUp.initialize({
             icon: GRAY_ICON,
             text: 'New Drawing',
             callback: cardButtonCallback
+        }, {
+            icon: GRAY_ICON,
+            text: 'Attach from link',
+            callback: cardButtonCallback
         }];
     },
 
