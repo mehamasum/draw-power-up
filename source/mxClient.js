3aab40184e6e6d8163e71cc820787154ad6f377f
mehamasum <mehamasum@gmail.com>
2017-06-25 15:27:28 +0600

    misc fix

--- source/mxClient.js
+++ source/mxClient.js
@@ -44959,7 +44959,7 @@ mxStylesheet.prototype.createDefaultVertexStyle = function()
 	style[mxConstants.STYLE_PERIMETER] = mxPerimeter.RectanglePerimeter;
 	style[mxConstants.STYLE_VERTICAL_ALIGN] = mxConstants.ALIGN_MIDDLE;
 	style[mxConstants.STYLE_ALIGN] = mxConstants.ALIGN_CENTER;
-	style[mxConstants.STYLE_FILLCOLOR] = '#C3D9FF';
+	style[mxConstants.STYLE_FILLCOLOR] = '#FFFFFF';
 	style[mxConstants.STYLE_STROKECOLOR] = '#6482B9';
 	style[mxConstants.STYLE_FONTCOLOR] = '#774400';
 	
